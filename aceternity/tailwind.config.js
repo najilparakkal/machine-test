16fde806ba83b7fcd96dafa014fbcd0db58e34b5
Najil Parakkal <najilparakkal@gmail.com>
2024-10-16 22:02:40 +0530

    landing

--- aceternity/tailwind.config.js
+++ aceternity/tailwind.config.js
@@ -1,54 +1,57 @@
-// tailwind.config.js
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 const flattenColorPalette = require("tailwindcss/lib/util/flattenColorPalette").default;
 
-
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}", 
-      "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-      "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        keyframes: {
-          move: {
-            "0%": { transform: "translateX(-200px)" },
-            "100%": { transform: "translateX(200px)" },
-          },
-          slideInFromLeft: {
-            '0%': { transform: 'translateX(-100%)', opacity: '0' },
-            '100%': { transform: 'translateX(0)', opacity: '1' },
-          },
-          warpSpeed: {
-            '0%': { backgroundPosition: '0% 0%' },
-            '50%': { backgroundPosition: '100% 100%' },
-            '100%': { backgroundPosition: '0% 0%' },
-          },
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        move: {
+          "0%": { transform: "translateX(-200px)" },
+          "100%": { transform: "translateX(200px)" },
         },
-        animation: {
-          slideInFromLeft: 'slideInFromLeft 1s ease-out forwards',
-          'warp-speed': 'warpSpeed 5s linear infinite',
-          move: "move 5s linear infinite",
+        slideInFromLeft: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
         },
-        backgroundSize: {
-          '300%': '300% 300%',
+        warpSpeed: {
+          '0%': { backgroundPosition: '0% 0%' },
+          '50%': { backgroundPosition: '100% 100%' },
+          '100%': { backgroundPosition: '0% 0%' },
+        },
+        loopCircle: {
+          '0%': { transform: 'scale(0)', opacity: 0 },
+          '50%': { transform: 'scale(1)', opacity: 1 },
+          '100%': { transform: 'scale(1.5)', opacity: 0 }, // Scales and fades out
         },
       },
+      animation: {
+        slideInFromLeft: 'slideInFromLeft 1s ease-out forwards',
+        'warp-speed': 'warpSpeed 5s linear infinite',
+        move: "move 5s linear infinite",
+        loopCircle: 'loopCircle 3s ease-in-out infinite',
+      },
+      backgroundSize: {
+        '300%': '300% 300%',
+      },
     },
-    plugins: [addVariablesForColors],    
-  };
-  
+  },
+  plugins: [addVariablesForColors],
+};
 
-  function addVariablesForColors({ addBase, theme }) {
-    const allColors = flattenColorPalette(theme("colors"));
-    const newVars = Object.fromEntries(
-      Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-    );
-  
-    addBase({
-      ":root": newVars,
-    });
-  }
+function addVariablesForColors({ addBase, theme }) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
